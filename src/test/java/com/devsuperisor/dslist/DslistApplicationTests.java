ac315b86a9d04998695d2c4ddf236c0e15df4752
Gabrielly Breganhola Pisaneschi <97494920+GabriellyBP@users.noreply.github.com>
2023-05-13 01:02:45 -0300

    Add files via upload

---
+++ src/test/java/com/devsuperisor/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperisor.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
